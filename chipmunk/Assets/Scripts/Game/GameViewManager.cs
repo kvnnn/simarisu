4ee43520e32aed74048b16c23f685e50742ed47e
Kevin Saito <yum02.skullcandy@gmail.com>
2015-10-03 15:29:25 +0900

    add game manager (not done at all)

--- chipmunk/Assets/Scripts/Game/GameViewManager.cs
+++ chipmunk/Assets/Scripts/Game/GameViewManager.cs
@@ -4,4 +4,16 @@ using System.Collections.Generic;
 
 public class GameViewManager : ViewManager
 {
+	[SerializeField]
+	private GameManager gameManager;
+
+	protected override void BeforeShow()
+	{
+		gameManager.PrepareGame();
+	}
+
+	protected override void AfterShow()
+	{
+		gameManager.StartGame();
+	}
 }
